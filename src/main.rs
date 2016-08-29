d62368b9b4083d337b6ba372728863899c648d3e
Casey Primozic <me@ameo.link>
2016-08-29 03:34:04 -0500

    Cleaned up code, fixing syntax errors and moving towards working version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 //! Algobot 4 Optimizer
 //! Created by Casey Primozic 2016-2016
 
-#![feature(conservative_impl_trait)] 
+#![feature(conservative_impl_trait)]
 
+extern crate uuid;
 extern crate postgres;
 extern crate redis;
 extern crate futures;
 
+extern crate algobot_util;
+
 mod transport;
+mod conf;
 
 fn main() {
 
--- src/transport/command_server.rs
+++ src/transport/command_server.rs
@@ -7,17 +7,24 @@
 //! Workers register interest after sending a command so that they can be notified
 //! of the successful reception of the command.
 
-//! TODO: Ensure that commands aren't processed twice by storing UIDs or most
+//! TODO: Ensure that commands aren't processed twice by storing Uuids or most
 //! recent 200 commands or something and checking that list before executing (?)
 
 //! TODO: Use different channel for responses than for commands
 
 use std::collections::VecDeque;
 use std::thread;
+use std::time::Duration;
 use std::sync::{Arc, Mutex};
 
 use futures::stream::{Stream, channel, Sender, Receiver};
 use futures::{Future, oneshot, Complete};
+use uuid::Uuid;
+use redis;
+
+use algobot_util::transport::redis::{get_client, sub_channel};
+
+use conf::CONF;
 
 type SenderQueue = Arc<Mutex<VecDeque<Sender<(Command, Complete<()>), ()>>>>;
 type CommandQueue = Arc<Mutex<VecDeque<Command>>>;
@@ -25,7 +32,8 @@ type CommandQueue = Arc<Mutex<VecDeque<Command>>>;
 /// Blocks the current thread until a Duration+Complete is received.
 /// Then it sleeps for that Duration and Completes the oneshot upon awakening.
 fn init_sleeper(rx: Receiver<(Duration, Complete<()>), ()>) {
-    for (dur, comp) in rx.wait() {
+    for res in rx.wait() {
+        let (dur, comp) = res.unwrap();
         thread::sleep(dur);
         comp.complete(());
     }
@@ -39,45 +47,69 @@ struct AlertList {
     channel_rx: Receiver<String, ()>,
     // Vec to hold the ids of responses we're waiting for and `Complete`s
     // to send the result back to the worker thread
-    list: Arc<Mutex<Vec<(UID, Complete<String, ()>)>>>
+    list: Vec<(Uuid, Complete<Result<WrappedResponse, ()>>)>
+}
+
+/// Represents a command sent to the Tick Processor
+enum Command {
+
+}
+
+/// Represents a command bound to a unique identifier that can be
+/// used to link it with a Response
+struct WrappedCommand {
+    uuid: Uuid,
+    cmd: Command
 }
 
 /// Represents a response from the Tick Processor to a Command sent
 /// to it at some earlier point.
 enum Response {
+    Ok,
+    Error{status: String}
+}
 
+struct WrappedResponse {
+    pub uuid: Uuid,
+    pub cmd: Response
 }
 
 impl AlertList {
     pub fn new() -> AlertList {
         let al = AlertList {
-            channel_rx: sub_channel(CONF.redis_response_channel),
+            channel_rx: sub_channel(CONF.redis_host, CONF.redis_response_channel),
             list: Vec::new()
         };
         al.listen()
     }
 
-    /// Register interest in Results with a specified UID and send
+    /// Register interest in Results with a specified Uuid and send
     /// the Result over the specified Oneshot when it's received
-    pub fn register(&mut self, response_uid: UID, c: Complete<String, ()>) {
-        self.list.push((response_uid, c));
+    pub fn register(&mut self, response_uuid: Uuid, c: Complete<Result<WrappedResponse, ()>>) {
+        self.list.push((response_uuid, c));
+    }
+
+    /// Deregisters a listener if a timeout in the case of a timeout occuring
+    pub fn deregister(&mut self, response_uuid: Uuid) {
+
     }
 
     /// Start listening on the channel and doling out Responses where requested
-    fn listen(mut self) {
-        let mut list = self.list.clone();
+    fn listen(mut self) -> AlertList {
         self.channel_rx.and_then(move |raw_res| {
             // TODO: Parse the Response into a Response object
-            self.send_messages(&*list, parsed_res);
+            self.send_messages(parsed_res);
+            Ok(())
         }).forget();
         self
     }
 
-    /// Send out the Response to any workers that registered interest ot its UID
-    fn send_messages(list: &Mutex<Vec<(UID, Complete<String, ()>)>>, res: Response) {
-        for (uid, c) in list {
-            if Response.uid == uid {
-                c.complete(res);
+    /// Send out the Response to any workers that registered interest ot its Uuid
+    fn send_messages(&mut self, list:  res: WrappedResponse) {
+        for (i, &(Uuid, c)) in self.list.iter().enumerate() {
+            if res.uuid == Uuid {
+                self.list.remove(i);
+                c.complete(Ok(res));
                 break;
             }
         }
@@ -88,7 +120,7 @@ pub struct CommandServer {
     conn_count: usize, // how many connections to open
     command_queue: CommandQueue, // internal command queue
     conn_queue: SenderQueue, // senders for idle command-sender threads
-
+    alert_list: Arc<Mutex<AlertList>> // vec of handles to workers waiting for particular Responses
 }
 
 /// Locks the CommandQueue and returns a queued command, if there are any.
@@ -99,33 +131,63 @@ fn try_get_new_command(command_queue: CommandQueue) -> Option<Command> {
 
 /// Asynchronously sends off a command to the Tick Processor without
 /// waiting to see if it was received or sent properly
-fn execute_command(command: Command, client: RedisClient) {
-    let _ = client.execute(command.as_str(), &[]);
+fn execute_command(cmd: WrappedCommand, client: &redis::Client) {
+    // TODO: Send command over redis
+}
+
+/// Returns a WrappedCommand that binds a UUID to a command so that
+/// Responses can be matched to it
+fn wrap_command(cmd: Command) -> WrappedCommand {
+    WrappedCommand {
+        uuid: Uuid::new_v4(),
+        cmd: cmd
+    }
 }
 
 /// Creates a command processor that awaits requests
-fn init_command_processor(rx: Receiver<(Command, Complete<()>), ()>, command_queue: CommandQueue) {
+fn init_command_processor(cmd_rx: Receiver<(Command, Complete<()>), ()>,
+        command_queue: CommandQueue, al: &Mutex<AlertList>) {
     // get a connection to the postgres database
-    let client = get_client().expect("Couldn't create postgres connection.");
+    let client = get_client(CONF.redis_host);
     // channel for communicating with the sleeper thread
     let (tx, rx) = channel::<(Duration, Complete<()>), ()>();
     thread::spawn(move || init_sleeper(rx) );
-    // Handler for new commands from the main thread
-    for tup in rx.wait() {
+    for tup in cmd_rx.wait() {
         let (cmd, done_tx) = tup.unwrap();
-        // send off the command into the great beyond
-        execute_command(cmd, &client);
+        // create a Uuid and bind it to the command
+        let wrapped_cmd = WrappedCommand{uuid: Uuid::new_v4(), cmd: cmd};
+        execute_command(wrapped_cmd, &client);
+        // `timeout` fulfills when the timeout is up
+        let (c, timeout) = oneshot::<()>();
         // start the timeout timer on a separate thread
         let timeout_promise = tx.send(Ok((CONF.command_timeout_duration, c)));
-        // TODO: register interest in command
-        timeout_promise.select(response_promise).then(|status| {
+        // TODO: Recycle tx
+        // oneshot for sending the Response back
+        let (complete, oneshot) = oneshot::<Result<WrappedResponse, ()>>();
+        let mut al_inner = al.lock().unwrap();
+        // register interest in new Responses coming in with our Command's Uuid
+        al_inner.register(wrapped_cmd.uuid, complete);
+        let mut attempts = 0;
+        timeout.select(oneshot).and_then(move |status| {
+            // Result received before the timeout
+            match status.unwrap() {
+                // command received
+                Ok(raw_res) => {
+                    // TODO: Parse into WrappedResponse + Result and return
+                    al_inner
+                },
+                // timed out
+                Err(_) => {
+                    // TODO
+                }
+            }
             // TODO: re-send command if timeout triggered
             // TODO: move on if it was successfully received
             // TODO: Clean out interest list if timed out
         }).wait(); // block until a response is received or the command times out
         // keep trying to get queued commands to execute until the queue is empty
         while let Some(new_command) = try_get_new_command(command_queue.clone()) {
-            execute_command(new_command, &client);
+            execute_command(wrap_command(new_command), &client);
         }
         // Let the main thread know it's safe to use the sender again
         // This essentially indicates that the worker thread is idle
@@ -137,12 +199,14 @@ impl CommandServer {
     pub fn new(conn_count: usize) -> CommandServer {
         let mut conn_queue = VecDeque::with_capacity(conn_count);
         let command_queue = Arc::new(Mutex::new(VecDeque::new()));
+        let al = Arc::new(Mutex::new(AlertList::new()));
         for _ in 0..conn_count {
+            let al = al.clone();
             // channel for getting the Sender back from the worker thread
             let (tx, rx) = channel::<(Command, Complete<()>), ()>();
 
             let qq_copy = command_queue.clone();
-            thread::spawn(move || init_command_processor(rx, qq_copy) );
+            thread::spawn(move || init_command_processor(rx, qq_copy, &*al) );
             // store the sender which can be used to send queries
             // to the worker in the connection queue
             conn_queue.push_back(tx);
@@ -151,7 +215,8 @@ impl CommandServer {
         CommandServer {
             conn_count: conn_count,
             command_queue: command_queue,
-            conn_queue: Arc::new(Mutex::new(conn_queue))
+            conn_queue: Arc::new(Mutex::new(conn_queue)),
+            alert_list: al
         }
     }
 
@@ -169,10 +234,10 @@ impl CommandServer {
             let tx = self.conn_queue.lock().unwrap().pop_front().unwrap();
             let cq_clone = self.conn_queue.clone();
             // future for notifying main thread when command is done and worker is idle
-            let (c, o) = oneshot::<()>();
-            tx.send(Ok((command, c))).and_then(|new_tx| {
+            let (complete, oneshot) = oneshot::<()>();
+            tx.send(Ok((command, complete))).and_then(|new_tx| {
                 // Wait until the worker thread signals that it is idle
-                o.and_then(move |_| {
+                oneshot.and_then(move |_| {
                     // Put the Sender for the newly idle worker into the connection queue
                     cq_clone.lock().unwrap().push_back(new_tx);
                     Ok(())
