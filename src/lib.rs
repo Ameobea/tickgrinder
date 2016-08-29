94dde4c4411246e0c85a1bf03864b85bb8f9108c
Casey Primozic <me@ameo.link>
2016-08-29 02:40:49 -0500

    Fleshed out redis sub_channel command

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 //! Utility functions for use by all parts of the algobot trading system
 
+extern crate redis;
+extern crate futures;
+
 pub mod transport;
--- src/transport/redis.rs
+++ src/transport/redis.rs
@@ -1,7 +1,14 @@
 //! Functions for interfacing with Redis
 
+use std::thread;
+
 use redis;
-use futures;
+use futures::Future;
+use futures::stream::{channel, Sender, Receiver};
+
+pub fn get_client(host: &str) -> redis::Client {
+    redis::Client::open(host).expect("Could not connect to redis")
+}
 
 /// Blocks until a message is received on a pubsub then returns it
 /// Returns the message as a String
@@ -16,17 +23,28 @@ fn get_message_outer(tx: Sender<String, ()>, ps: &redis::PubSub) {
     let res = get_message(ps);
     // block again until the message is consumed
     // this prevents the tx from dropping since .send() is async
-    tx.send(Ok(res)).wait().map(|new_tx| {
+    let _ = tx.send(Ok(res)).wait().map(|new_tx| {
         // start listening again
         get_message_outer(new_tx, ps);
     });
 }
 
-pub fn get_pubsub(channel: &'static str) -> redis::PubSub {
-    let client = get_client();
+pub fn get_pubsub(host: &str, channel: &'static str) -> redis::PubSub {
+    let client = get_client(host);
     let mut pubsub = client.get_pubsub()
         .expect("Could not create pubsub for redis client");
     pubsub.subscribe(channel)
         .expect("Could not subscribe to pubsub channel");
     pubsub
+}
+
+// Returns a Receiver that resolves to new messages received
+// on a pubsub channel
+pub fn sub_channel(host: &str, ps_channel: &'static str) -> Receiver<String, ()> {
+    let (tx, rx) = channel::<String, ()>();
+    let ps = get_pubsub(host, ps_channel);
+    thread::spawn(move || {
+        get_message_outer(tx, &ps);
+    });
+    rx
 }
