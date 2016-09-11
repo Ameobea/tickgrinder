d1a26df880b03a4fe337672a3ce120925bcdd1fd
Casey Primozic <me@ameo.link>
2016-09-10 20:27:40 -0500

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+//! # Channel Identification via Sliding Windows
+//!
+//! This strategy aims to locate price channels over arbitrary time periods by analyzing the
+//! minimums and maximums of prices over various time periods.  By determining new lows of a
+//! macro trend and watching for new highs in smaller time periods, subtrends and the channels
+//! that they make up can be located and analyzed for trade opportunities.
+
+extern crate futures;
+extern crate algobot_util;
+
+use futures::Complete;
+
+use algobot_util::strategies::Strategy;
+use algobot_util::tick::SymbolTick;
+
+struct strat{}
+
+impl Strategy for strat {
+    fn process(&mut self, t: SymbolTick) {
+
+    }
+
+    fn exit_now(&mut self, ready: futures::Complete<()>) {
+
+    }
+}
