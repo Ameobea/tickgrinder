166e1b6312a8acd67ea4de230d915d8057a10170
Casey Primozic <me@ameo.link>
2016-09-12 22:58:52 -0500

    Boilerplate for strategy initialization

--- channel_id_sliding_windows/src/lib.rs
+++ channel_id_sliding_windows/src/lib.rs
@@ -5,7 +5,7 @@
 //! macro trend and watching for new highs in smaller time periods, subtrends and the channels
 //! that they make up can be located and analyzed for trade opportunities.
 
-#![feature(test)]
+#![feature(test, conservative_impl_trait)]
 
 extern crate futures;
 extern crate algobot_util;
@@ -14,13 +14,33 @@ extern crate test;
 mod window_manager;
 
 use futures::Complete;
-
 use algobot_util::strategies::Strategy;
 use algobot_util::tick::SymbolTick;
+use algobot_util::transport::command_server::CommandServer;
+use algobot_util::transport::query_server::QueryServer;
 
-struct SldingWindows{}
+use window_manager::WindowManager;
 
-impl Strategy for SldingWindows {
+struct SlidingWindows {
+    wm: WindowManager
+}
+
+impl SlidingWindows {
+    fn new() -> SlidingWindows {
+        SlidingWindows {
+            wm: WindowManager::new()
+        }
+    }
+}
+
+impl Strategy for SlidingWindows {
+    /// This should initialize the strategy and start it running.  It should be asynchronous so
+    /// the strategy doesn't block the main thread, so it's likely that the strategy should run
+    /// on a separate thread.
+    fn init(cs: CommandServer, qs: QueryServer) {
+        let strat = SlidingWindows::new();
+    }
+
     fn process(&mut self, t: SymbolTick) {
 
     }
