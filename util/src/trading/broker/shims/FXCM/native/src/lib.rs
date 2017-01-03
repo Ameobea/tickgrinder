510495617df17569b98f42a2c09e4258c47c2360
Casey Primozic <me@ameo.link>
2017-01-02 22:31:03 -0600

    Worked more on FFI stuff with the Native broker server  - Implemented some tests and benchmarks  - Dealt with potential future memory leaks and handling Strings from C.

--- util/src/trading/broker/shims/FXCM/native/src/lib.rs
+++ util/src/trading/broker/shims/FXCM/native/src/lib.rs
@@ -1,22 +1,24 @@
 //! Broker shim for FXCM.
 
-#![feature(libc)]
+#![feature(libc, test)]
 #![allow(dead_code, unused_imports, unused_variables)]
 
 extern crate uuid;
 extern crate futures;
 extern crate algobot_util;
 extern crate libc;
+extern crate test;
 
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::ptr::null;
 use std::thread;
 use std::mem::{self, transmute};
-use std::sync::mpsc;
 use std::time::Duration;
+use std::slice;
+use std::str;
 
-use libc::{c_char, c_void, uint64_t, c_double};
+use libc::{c_char, c_void, uint64_t, c_double, memchr};
 use uuid::Uuid;
 use futures::Oneshot;
 use futures::sync::oneshot;
@@ -101,7 +103,7 @@ pub struct ClientMessage {
 pub struct FXCMNative {
     settings_hash: HashMap<String, String>,
     server_environment: *mut c_void,
-    raw_rx: Option<mpsc::Receiver<BrokerResult>>,
+    raw_rx: Option<UnboundedReceiver<BrokerResult>>,
 }
 
 // something to hold our environment so we can convince Rust to send it between threads
@@ -129,7 +131,7 @@ pub extern fn tick_downloader_cb(timestamp: uint64_t, bid: uint64_t, ask: uint64
 /// stream returned by `get_stream`.
 extern fn handle_message(tx_ptr: *mut c_void, message: *mut ServerMessage) {
     unsafe {
-        let sender: &mut mpsc::Sender<BrokerResult> = transmute(tx_ptr);
+        let sender: &mut UnboundedSender<BrokerResult> = transmute(tx_ptr);
         let res: BrokerResult = match (*message).response {
             ServerResponse::POSITION_OPENED => {
                 unimplemented!();
@@ -139,11 +141,28 @@ extern fn handle_message(tx_ptr: *mut c_void, message: *mut ServerMessage) {
                 let msg = BrokerMessage::Pong{time_received: *micros};
                 Ok(msg)
             },
+            ServerResponse::ERROR => {
+                let msg_ptr: *mut c_char = (*message).payload as *mut c_char;
+                let err_msg: CString = ptr_to_cstring(msg_ptr);
+                Err(BrokerError::Message{message: err_msg.into_string()
+                    .expect("Unable to convert CString ito String")})
+            },
             _ => unimplemented!(),
         };
 
+        libc::free((*message).payload);
+
         let _ = sender.send(res);
     }
+}
+
+/// Takes a pointer to a string from C and copies it into a Rust-owned CString.
+unsafe fn ptr_to_cstring(ptr: *mut c_char) -> CString {
+    // expect that no strings are longer than 100000 bytes
+    let end_ptr = memchr(ptr as *const c_void, 0, 100000);
+    let len: usize = end_ptr as usize - ptr as usize;
+    let slice: &[u8] = slice::from_raw_parts(ptr as *const u8, len);
+    CString::new(slice).expect("Unable to convert the slice into a CString")
 }
 
 impl Broker for FXCMNative {
@@ -151,7 +170,7 @@ impl Broker for FXCMNative {
         let (ext_tx, ext_rx) = oneshot::channel::<Result<Self, BrokerError>>();
         thread::spawn(move || {
             // channel with which to receive messages from the server
-            let (mut tx, rx) = mpsc::channel::<BrokerResult>();
+            let (mut tx, rx) = unbounded::<BrokerResult>();
             let tx_ptr = &mut tx as *const _ as *mut c_void;
 
             let server_environment: *mut c_void = unsafe { init_server_environment(Some(handle_message), tx_ptr) };
@@ -188,15 +207,11 @@ impl Broker for FXCMNative {
     }
 
     fn get_stream(&mut self) -> Result<UnboundedReceiver<BrokerResult>, BrokerError> {
-        let (mut ext_tx, ext_rx) = unbounded::<BrokerResult>();
-        let raw_rx = mem::replace::<Option<mpsc::Receiver<BrokerResult>>>(&mut self.raw_rx, None).unwrap();
-        thread::spawn(move || {
-            for res in raw_rx.iter() {
-                let _ = ext_tx.send(res);
-            }
-        });
-
-        Ok(ext_rx)
+        if self.raw_rx.is_some() {
+            return Ok(mem::replace::<Option<UnboundedReceiver<BrokerResult>>>(&mut self.raw_rx, None).unwrap())
+        } else {
+            return Err(BrokerError::Message{message: String::from("The stream for this broker has already been taken.")})
+        }
     }
 
     fn sub_ticks(&mut self, symbol: String) -> Result<Box<Stream<Item=Tick, Error=()> + Send>, BrokerError> {
@@ -234,7 +249,7 @@ fn login_test() {
 #[test]
 fn broker_server() {
     // channel with which to receive responses from the server
-    let (tx, rx) = mpsc::channel::<BrokerResult>();
+    let (tx, rx) = unbounded::<BrokerResult>();
     let tx_ptr = &tx as *const _ as *mut c_void;
 
     let env: *mut c_void = unsafe { init_server_environment(Some(handle_message), tx_ptr) };
@@ -255,13 +270,73 @@ fn broker_server() {
 
     thread::spawn(move || {
         for i in 0..10 {
-            println!("Sending message on rust side...");
             unsafe { push_client_message(message.clone(), ship2.0) };
-            thread::sleep(Duration::from_millis(10));
+            thread::sleep(Duration::from_millis(1));
         }
+        unsafe { push_client_message(ClientMessage {
+            command: transmute(100u32), // invalid enum variant to trigger an error on the C side
+            payload: 0 as *mut c_void,
+        }, ship2.0)}
     });
 
-    for res in rx.iter().take(10) {
-        println!("Response received from server: {:?}", res);
+    for (i, res) in rx.wait().take(11).enumerate() {}
+}
+
+#[test]
+fn cstring_conversion() {
+    unsafe {
+        // allocate a native c-string using system allocator
+        let s: &[u8] = b"An example str.\x00";
+        let ptr = libc::malloc(16) as *mut c_char;
+        libc::strncpy(ptr, s.as_ptr() as *const c_char, s.len());
+        let res: CString = ptr_to_cstring(ptr);
+        assert_eq!(res.to_str().unwrap(), str::from_utf8(&s[0..15]).unwrap());
+        libc::free(ptr as *mut c_void);
+    }
+}
+
+#[bench]
+fn ptr_to_cstring_16(b: &mut test::Bencher) {
+    unsafe {
+        let s: &[u8] = b"An example str.\x00";
+        let ptr = libc::malloc(16) as *mut c_char;
+        libc::strncpy(ptr, s.as_ptr() as *const c_char, s.len());
+        b.iter(|| {
+            let _ = ptr_to_cstring(ptr);
+        });
+        // free the string because it's the right thing to do
+        libc::free(ptr as *mut c_void);
+    }
+}
+
+#[bench]
+fn ptr_to_cstring_64(b: &mut test::Bencher) {
+    unsafe {
+        let s: &[u8] = b"An example string that continues on much longer than before....\x00";
+        let ptr = libc::malloc(64) as *mut c_char;
+        libc::strncpy(ptr, s.as_ptr() as *const c_char, s.len());
+        b.iter(|| {
+            let _ = ptr_to_cstring(ptr);
+        });
+        // free the string because it's the right thing to do
+        libc::free(ptr as *mut c_void);
+    }
+}
+
+#[bench]
+fn ptr_to_cstring_1024(b: &mut test::Bencher) {
+    unsafe {
+        let mut v = Vec::with_capacity(1024);
+        for _ in 0..1024 {
+            v.push(69u8);
+        }
+        let s = v.as_slice();
+        let ptr = libc::malloc(1024) as *mut c_char;
+        libc::strncpy(ptr, s.as_ptr() as *const c_char, s.len());
+        b.iter(|| {
+            let _ = ptr_to_cstring(ptr);
+        });
+        // free the string because it's the right thing to do
+        libc::free(ptr as *mut c_void);
     }
 }
