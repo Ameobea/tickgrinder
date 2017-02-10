6d80dba8d4f414fe6967a84f4d3ea0b233f4d384
Casey Primozic <me@ameo.link>
2017-02-09 22:13:10 -0600

    Added filtering by instance for the React MM log page

--- mm-react/src/components/logging/LiveLog.js
+++ mm-react/src/components/logging/LiveLog.js
@@ -1,15 +1,10 @@
 //! A live view of log messages.
 
 import { connect } from 'dva';
-import { Row, Col, AutoComplete, Card, Tag, Checkbox } from 'antd';
+import { Row, Col, AutoComplete, Card, Tag, Checkbox, Tooltip } from 'antd';
 
 import styles from '../../static/css/logging.css';
-import { LogLine, Severity } from './LogLine';
-
-/// 
-const getDataSource = (log_cache) => {
-
-}
+import { LogLine, Severity, Instance } from './LogLine';
 
 const handleSeverityClose = (dispatch, name) => {
   dispatch({type: 'logging/severityRemoved', item: name});
@@ -19,7 +14,11 @@ const handleCategoryClose = (dispatch, name) => {
   dispatch({type: 'logging/categoryClosed', item: name});
 }
 
-const SelectedTags = connect()(({dispatch, selected_categories, selected_severities, inclusive}) => {
+const handleInstanceClose = (dispatch, sender) => {
+  dispatch({type: 'logging/instanceClosed', item: sender});
+}
+
+const SelectedTags = connect()(({dispatch, selected_categories, selected_severities, selected_instances, inclusive}) => {
   let tags = [];
   for(var i=0; i<selected_severities.length; i++) {
     let name = selected_severities[i];
@@ -40,6 +39,18 @@ const SelectedTags = connect()(({dispatch, selected_categories, selected_severit
     tags.push(tag);
   }
 
+  for(var i=0; i<selected_instances.length; i++) {
+    let sender = selected_instances[i];
+    let tag = (
+      <Tooltip placement="right" title={sender.uuid} key={'instance-' + sender.uuid}>
+        <Tag closable color="green" onClick={() => handleInstanceClose(dispatch, sender)}>
+          {sender.instance_type}
+        </Tag>
+      </Tooltip>
+    );
+    tags.push(tag);
+  }
+
   return (
     <Card>
       <Checkbox onChange={(e) => dispatch({type: 'logging/toggleMatch'})} checked={inclusive}>
@@ -53,13 +64,14 @@ const SelectedTags = connect()(({dispatch, selected_categories, selected_severit
   );
 });
 
-const LiveLog = ({dispatch, log_cache, selected_categories, selected_severities, inclusive}) => {
+const LiveLog = ({dispatch, log_cache, selected_categories, selected_severities, selected_instances, inclusive}) => {
   let rows = [];
   for(let i = log_cache.length - 1; i > (log_cache.length - 26) && i >= 0; i--) {
     var log_line = log_cache[i];
     // check if the log line should be displayed based on the selected categories/severities and inclusiveness state
     let contains = ((selected_severities.indexOf(log_line.cmd.Log.msg.level) != -1 == inclusive) || (selected_severities.length === 0)) &&
-      ((selected_categories.indexOf(log_line.cmd.Log.msg.message_type) != -1 == inclusive) || (selected_categories.length === 0));
+      ((selected_categories.indexOf(log_line.cmd.Log.msg.message_type) != -1 == inclusive) || (selected_categories.length === 0)) &&
+      (((selected_instances.filter(sender => sender.uuid != log_line.cmd.Log.msg.sender.uuid).length == 0) == inclusive) || (selected_instances.length === 0));
     if(contains) {
       rows.push(<LogLine key={log_line.uuid} msg={log_line.cmd.Log.msg} />);
     }
@@ -67,7 +79,12 @@ const LiveLog = ({dispatch, log_cache, selected_categories, selected_severities,
 
   return (
     <div className={styles.liveLog}>
-      <SelectedTags selected_severities={selected_severities} selected_categories={selected_categories} inclusive={inclusive} />
+      <SelectedTags
+        selected_severities={selected_severities}
+        selected_categories={selected_categories}
+        selected_instances={selected_instances}
+        inclusive={inclusive}
+      />
       <Row>
         <Col span={2}><b>Sending Instance</b></Col>
         <Col span={2}><b>Event Type</b></Col>
@@ -84,6 +101,7 @@ function mapProps(state) {
     log_cache: state.platform_communication.log_messages,
     selected_categories: state.logging.selected_categories,
     selected_severities: state.logging.selected_severities,
+    selected_instances:  state.logging.selected_instances,
     inclusive: state.logging.inclusive,
   };
 }
