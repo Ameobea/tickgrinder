1780d64d6f5fefa3163a4b7641e84e3379460181
Casey Primozic <me@ameo.link>
2017-02-14 03:32:05 -0600

    SQUASH ME

---
+++ mm-react/src/components/MacroManager.js
@@ -0,0 +1,13 @@
+//! The macro manager supplies an interface for listing, creating, and executing spawner macros.  Spawner macros are the highest level of user
+//! control over the platform itself, allowing the automation of tasks such as instance spawning, strategy deployment, backtesting, and other high-
+//! level control over the platform and its modules.
+
+import { connect } from 'antd';
+
+const MacroManager = ({dispatch}) => {
+  return (
+
+  );
+}
+
+export default connect()(MacroManager);
