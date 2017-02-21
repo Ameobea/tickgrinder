e57e0d856c55960bb317282e242fc15b7b53cdcd
Casey Primozic <me@ameo.link>
2017-02-21 12:53:46 -0600

    Set up basis for data management page
     - Created components for donloading data and managing downloading data
     - Created `Data` model for handling state related to those components
     - Came up with some more clean and officialized ideas involving how to reason with data downloads

--- mm-react/src/routes/DataManagement.js
+++ mm-react/src/routes/DataManagement.js
@@ -3,10 +3,17 @@
 import React from 'react';
 
 import ContentContainer from '../components/ContentContainer';
+import DataDownloader from '../components/data/DataDownloader';
+import DataManager from '../components/data/DataManager';
 
 function DataManagement() {
   return (
-    <div>{'Data'}</div>
+    <div>
+      <h1>Data Downloader</h1>
+      <DataDownloader />
+      <h1>Data Manager</h1>
+      <DataManager />
+    </div>
   );
 }
 
