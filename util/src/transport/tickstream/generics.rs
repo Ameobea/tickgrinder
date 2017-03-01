5bd8612c99867ce8062eba1b79d7542a1c39fd93
Casey Primozic <me@ameo.link>
2017-03-01 17:39:52 -0600

    Added in basis for generic tick Gens, Maps, and Sinks.
     - Continued work on the IEX downloader and added some additional FFI functions for saving ticks to various sources.

---
+++ util/src/transport/tickstream/generics.rs
@@ -0,0 +1,58 @@
+//! Contains the definitions of generic tick processing objects.
+//!
+//! The general idea is that ticks are created or pulled from some external source in the `GenTickGenerator`s,
+//! are processed internally by the `GenTickMap`s possibly returning a different type of tick, and finally
+//! consumed by the `GenTickSink`s.
+
+// This is currently experimental (more so than the rest of the platform) and is not a certain implementation.
+
+// TODO: Organize the individual sinks, gens, maps into subdirectories
+
+use futures::sync::mpsc::Receiver;
+use serde::{Serialize, Deserialize};
+
+use trading::tick::GenTick;
+
+/// Represents an external source of data that can be used to produce ticks.
+pub trait GenTickGenerator<T> {
+    /// Retruns a `Stream` of `GenTick<T>`s from the generator.
+    fn get_stream(&mut self) -> Receiver<GenTick<T>>;
+}
+
+/// Represents a transformation applied to ticks.
+pub trait GenTickMap<T, O> {
+    /// Processes a tick through the map, optionally returning a new tick.
+    fn map(&mut self, t: GenTick<T>) -> Option<GenTick<O>>;
+}
+
+/// Represents a destination for ticks and serves as the endpoint of a tick processing pipeline.
+pub trait GenTickSink<T> {
+    /// Processes a tick into the sink.
+    fn tick(&mut self, t: GenTick<T>);
+}
+
+// some example implementations of generic tick sinks
+
+struct PostgresSink<T> {
+    buffer: Vec<GenTick<T>>,
+}
+
+// An example of specifically implementing this trait for a specific kind of data.  Since postgres requires that the
+// schema of data be known in order for it to be stored, the kind of generic tick in the implementation must also be known.
+impl GenTickSink<(usize, usize)> for PostgresSink<(usize, usize)> {
+    fn tick(&mut self, t: GenTick<(usize, usize)>) {
+        unimplemented!();
+    }
+}
+
+struct RedisChannelSink<T> {
+    buffer: Vec<GenTick<T>>,
+}
+
+// An example of generically implementing these traits for a struct.  Since Redis doesn't care the schema of the data
+// you send over it (as long as you can make it into a string) any kind of generic tick is acceptable.
+impl<T> GenTickSink<T> for RedisChannelSink<T> where T:Serialize, T:Deserialize {
+    fn tick(&mut self, t: GenTick<T>) {
+        unimplemented!();
+    }
+}
