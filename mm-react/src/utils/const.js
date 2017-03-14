a1cccbca4b93a9da54b463ede83b15d1c4818f4d
Casey Primozic <me@ameo.link>
2017-03-14 01:30:43 -0500

    SQUASH ME
     - Fleshed out more of the `TickSink` function, fixed some types and defintion structures, and cleaned up some unused code

--- mm-react/src/utils/const.js
+++ mm-react/src/utils/const.js
@@ -40,27 +40,62 @@ const dataDownloaders: Array<DataDownloaderDefinition> = [
 /**
  * `TickSink`s are react components that contain an extra function that, using the state of the component, creates a `HistTickDst`
  * that can be used along with a backtest.
+ *
+ * Use like `<TickSink name='Example Sink' def={tickSinkDefs.Flatfile} />`
+ *
+ * These components also compose a `getHistTickDst()` method that returns a JSON-encoded `HistTickDst` or null if the user neglected
+ * to input one or more of the required input fields.
  */
 class TickSink extends React.Component {
-  constructor(props) {
+  constructor(props: {name: string, jsonName: string, defs: Array<{paramName: string, paramType: string}>}) {
     super(props);
     // create `TickSink`s out of all of the supplied sink definitions
-    let defs = props.paramDefs;
-    const sinks = defs.map((def: {paramName: string, paramType: string}): TickSinkParam<any> => tickSinkParams[def.paramType](def.paramName));
+    let defs = props.defs;
+    const sinks = defs.map((def: {paramName: string, paramType: string}): TickSinkParam<any> => tickSinkParamGens[def.paramType](def.paramName));
 
     this.state = {
       sinks: sinks,
     };
   }
 
-  render() {
+  /**
+   * Retrieves the values of all of the sink's parameters, collectes them into an object, and returns it as a JSON-encoded string.
+   * This should be able to be included in a `DownloadTicks` or similar command.  If any of the parameters are missing values,
+   * this function returns `null`.
+   */
+  getHistTickDst() {
+    let dst = {};
+    // merge the props of all the parameters into one object
+    for(var i=0; i<this.state.sinks.length; i++) {
+      dst = Object.assign(dst, this.state.sinks[i]);
+    }
 
+    // return an early null if any of the parameters are null (due to the user not filling them out)
+    for(var key in dst) {
+      if(dst[key] === null) {
+        return null;
+      }
+    }
+
+    // convert the dst to format `{SinkName: {...}}` and return as a JSON-encoded string
+    let finalDestination = {}; // ;)
+    finalDestination[this.props.jsonName] = dst;
+    return JSON.stringify(finalDestination);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.name}</h2>
+        {this.state.sinks}
+      </div>
+    );
   }
 }
 
 TickSink.PropTypes = {
   name: React.PropTypes.string,
-  paramDefs: React.PropTypes.arrayOf(
+  defs: React.PropTypes.arrayOf(
     React.PropTypes.shape({
       paramName: React.PropTypes.string,
       paramType: React.PropTypes.string,
@@ -76,29 +111,41 @@ type TickSinkParam<T> = React.Component & {getValue: () => ?T};
 /**
  * A function that, given the name of the parameter, returns a `TickSinkParam` for it.
  */
-type TickSinkParamGenerator<T> = (name: string) => TickSinkParam<T>;
+type TickSinkParamGenerator<T> = (clearName: string, jsonName: string) => TickSinkParam<T>;
 
 /**
  * All parameter generator functions available for tick sinks to use in their components
  */
 const tickSinkParamGens: { [key: string]: TickSinkParamGenerator<any> } = {
-  str: (name: string): TickSinkParam<string> => {
+  str: (clearName: string, jsonName: string): TickSinkParam<string> => {
     class StringInput extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          input: <Input />,
+        };
+      }
+
       getValue(): ?string {
-        return 'TEST';
+        if(this.state.input.value == '') {
+          return null;
+        }
+
+        return JSON.parse(`{${this.props.jsonName}: ${this.state.input.value}}`);
       }
 
       render() {
         return (
-          <div>
-            {this.props.name}{':'} <Input />
+          <div key={clearName}>
+            {this.props.clearName}{':'} {this.state.input}
           </div>
         );
       }
     }
 
     StringInput.propTypes = {
-      name: React.PropTypes.string,
+      clearName: React.PropTypes.string,
+      jsonName: React.PropTypes.string,
     };
 
     return <StringInput name={name} />;
@@ -109,12 +156,22 @@ const tickSinkParamGens: { [key: string]: TickSinkParamGenerator<any> } = {
  * A set of definitions for props that represent all possible places that ticks can be sent to.  They contain the names of all paramaters that are required
  * in order to build the `HistTickDst`s internally.
  */
-const tickSinkDefs: { [key: string]: Array<{name: string, param: TickSinkParam}> } = {
+const tickSinkDefs: { [key: string]: Array<{clearName: string, paramType: TickSinkParam}> } = {
   Console: [],
-  Flatfile: [{name: 'Filename', param: tickSinkParamGens.str}],
+  Flatfile: [{clearName: 'Filename', param: 'str'}],
+  Postgres: [{clearName: 'Table', param: 'str'}],
+  RedisChannel: [
+    {clearName: 'Redis Host', param: 'str'},
+    {clearName: 'Channel', param: 'str'},
+  ],
+  RedisSet: [
+    {clearName: 'Redis Host', param: 'str'},
+    {clearName: 'Set Name', param: 'str'},
+  ]
 };
 
 export default {
   dataDownloaders: dataDownloaders,
-  tickSinks: tickSinks,
+  TickSink: TickSink,
+  tickSinkDefs: tickSinkDefs,
 };
